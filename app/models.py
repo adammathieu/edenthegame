978689afa537019116695916d91a9a60409291ce
Mathieu Adam <mathieu.adam@ingenico.com>
2018-06-20 13:52:14 +0200

    Update models

--- app/models.py
+++ app/models.py
@@ -6,3 +6,18 @@ class Factions(db.Model):
 
 	def __repr__(self):
 		return '<Faction {}>'.format(self.name)
+
+class Stigmates(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), index=True, unique=True)
+
+	def __repr__(self):
+		return '<Stigmate {}>'.format(self.name)
+
+class States(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), index=True, unique=True)
+	description = db.Column(db.Text, index=True, unique=True)
+
+	def __repr__(self):
+		return '<Stigmate {}:{}>'.format(self.name,self.description)
