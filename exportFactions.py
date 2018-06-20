be831f73fcacf041792a9af058e000dfb2ac6ab7
Mathieu Adam <mathieu.adam@ingenico.com>
2018-06-20 19:59:53 +0200

    Add export script for table Factions

---
+++ exportFactions.py
@@ -0,0 +1,9 @@
+from app import db
+from app.models import Factions
+
+
+factions = ['jokers','bamaka','matriarcat','convoi','isc','horde','resistance','askaris','nephilim','angesdedante','cnj']
+for faction in factions:
+	raw = Factions(name=faction)
+	db.session.add(raw)
+	db.session.commit()
