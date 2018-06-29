932800fbcd564d9473cb16dd1361be3d9376ea74
Mathieu Adam <adammathieu@gmail.com>
2018-06-29 17:09:29 -0400

    Update exportJson and test dropdown menu

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template
 from app import app
+from app import db
+from app.models import Factions,Stigmates,ProfilType
 
 @app.route('/')
 @app.route('/index')
@@ -7,10 +9,14 @@ def hello_world():
 	user = {'username': 'Zagan'}
 	return render_template('index.html', title='Eden The Game', user=user)
 
-@app.route('/profils')
+@app.route('/profils', methods=['GET','POST'])
 def profils():
 	user = {'username': 'Zagan'}
-	return render_template('profils.html', title='Eden The Game - Cartes de profils', user=user)
+	factionsList = Factions.query.all()
+	print(factionsList)
+	print(type(factionsList))
+	print(type(factionsList[0]))
+	return render_template('profils.html', title='Eden The Game - Cartes de profils', user=user, factions=factionsList)
 
 @app.route('/tactiques')
 def tactiques():
---
+++ edendata/profils/exportBamaka.py
@@ -0,0 +1,15 @@
+import os
+from app import db
+from app.models import Profils
+from exportjson import readJsonFile
+
+directory = os.fsencode("bamaka")
+for file in os.listdir(directory):
+	filename = os.fsdecode(file)
+	print(filename)
+	if filename.endswith(".json"):
+		data = readJsonFile(os.path.join("bamaka",filename))	#data is a list
+		for profil in data:
+			raw = Profils(profil=profil)
+			db.session.add(raw)
+			db.session.commit()
